978a7a492f092023af216534873c1de3b5742fdd
Anish <cybertronian217@gmail.com>
2022-09-13 18:23:51 +0530

    Added transaction functionality ✅

--- components/PingButton.tsx
+++ components/PingButton.tsx
@@ -3,18 +3,37 @@ import { FC, useState } from "react";
 import styles from "../styles/PingButton.module.css";
 import * as Web3 from "@solana/web3.js";
 
-const PROGRAM_ID = `ChT1B39WKLS8qUrkLvFDXMhEJ4F1XZzwUNHUt4AU9aVa`;
-const DATA_ACCOUNT_PUBKEY = `Ah9K7dQ8EHaZqcAsgBW8w37yN2eAy3koFmUn4x3CJtod`;
+const PROGRAM_ID = new Web3.PublicKey(
+  `ChT1B39WKLS8qUrkLvFDXMhEJ4F1XZzwUNHUt4AU9aVa`
+);
+const DATA_ACCOUNT_PUBKEY = new Web3.PublicKey(
+  `Ah9K7dQ8EHaZqcAsgBW8w37yN2eAy3koFmUn4x3CJtod`
+);
 
 export const PingButton: FC = () => {
   const { connection } = useConnection();
   const { publicKey, sendTransaction } = useWallet();
 
-  const onClick = () => {
+  const onClick: FC = () => {
     if (!connection || !publicKey) {
-      alert("Oops! Wallet not connected or missing public key");
+      alert("Wallet not connected or no public key");
       return;
     }
+    const transaction = new Web3.Transaction();
+    const instruction = new Web3.TransactionInstruction({
+      keys: [
+        {
+          pubkey: DATA_ACCOUNT_PUBKEY,
+          isSigner: false,
+          isWritable: true,
+        },
+      ],
+      programId: PROGRAM_ID,
+    });
+    transaction.add(instruction);
+    sendTransaction(transaction, connection).then((signature) => {
+      console.log(`https://explorer.solana.com/tx/${signature}?cluster=devnet`);
+    });
   };
 
   return (
